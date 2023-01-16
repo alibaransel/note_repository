432c7766553c5ba05b9d9ce6620fb6b531045967
alibaransel <alibaransel25@gmail.com>
2023-01-16 23:05:11 +0300

    Service model development

--- lib/models/service.dart
+++ lib/models/service.dart
@@ -1,11 +1,40 @@
 //TODO: Research and improve
 
+import 'package:flutter/foundation.dart';
+
 abstract class Service {}
 
-abstract class StartableService extends Service {
-  void start();
+abstract class InitableService extends Service with _Initable {}
+
+abstract class StoppableService extends Service with _Stoppable {}
+
+abstract class InitableStoppableService extends Service with _Initable, _Stoppable {}
+
+mixin _Initable on Service {
+  bool _initialized = false;
+
+  bool get isInitialized => _initialized;
+
+  @mustCallSuper
+  void init() {
+    _initialized = true;
+  }
 }
 
-abstract class StoppableService extends StartableService {
-  void stop();
+mixin _Stoppable on Service {
+  bool _running = false;
+
+  bool get isRunning => _running;
+
+  @protected
+  @mustCallSuper
+  void start() {
+    _running = true;
+  }
+
+  @protected
+  @mustCallSuper
+  void stop() {
+    _running = false;
+  }
 }
