d52d58a03a0df8a93c7bce51e17888b765a7d46f
alibaransel <alibaransel25@gmail.com>
2023-05-18 01:18:56 +0300

    Json serialization

--- lib/models/account.dart
+++ lib/models/account.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:note_repository/constants/app_keys.dart';
+import 'package:note_repository/constants/app_paths.dart';
+import 'package:note_repository/services/storage_service.dart';
 
 class Account {
   Account({
@@ -13,4 +16,21 @@ class Account {
   String email;
   Image image;
   String loginType;
+
+  Future<Account> fromJson(Map<String, dynamic> json) async {
+    return Account(
+      uid: json[AppKeys.uid]! as String,
+      name: json[AppKeys.name]! as String,
+      email: json[AppKeys.email]! as String,
+      image: await StorageService.file.getImage(AppPaths.userImage), //TODO: Don't cache image here
+      loginType: json[AppKeys.loginType]! as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        AppKeys.uid: uid,
+        AppKeys.name: name,
+        AppKeys.email: email,
+        AppKeys.loginType: loginType,
+      };
 }
